ad79814eed8029f33f708d5629dfb97845c0034c
Javier Ocampo <ferroxido@gmail.com>
2018-05-13 23:36:36 -0300

    Split schema and resolvers

--- server/src/entity/User.ts
+++ server/src/entity/User.ts
@@ -1,12 +1,10 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from "typeorm";
 
 @Entity()
-export class User {
+export class User extends BaseEntity {
   @PrimaryGeneratedColumn() id: number;
 
-  @Column() firstName: string;
+  @Column() email: string;
 
-  @Column() lastName: string;
-
-  @Column() age: number;
+  @Column() password: string;
 }
